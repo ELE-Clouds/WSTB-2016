fcc4f5ad37f5f2fe597cf1e6f914082f3d8b9113
hello_world <gt_china_bird@126.com>
2017-01-03 14:33:35 +0800

    Add files via upload
    增加一个文字编码模块，用于对部分搜索引擎需要需要将中文编码转成UTF-8编码（使用‘%’间隔）。如：搜库的。在搜索栏输入中文，在地址栏中可以看到一串使用%号间隔的两位十六进制编码。

---
+++ project/python/wstb_encode_mod.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#by Python2.7
+import urllib
+
+enType={'utf8':'UTF-8','gbk':'gbk','gb2312':'gb2312'}
+
+#utf-8编码
+def keyEncode(_strKey,_enCode):
+    #strKey1=unicode(_strKey,'gbk')  #注意，非控制台输入，此处无须添加，此处留作记录
+    params={}
+    params['']=_strKey.encode(enType[_enCode])
+    strKey = urllib.urlencode(params)
+    #strKey = 
+    return ''.join(strKey.split('=',1))
